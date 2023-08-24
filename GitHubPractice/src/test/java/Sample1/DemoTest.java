bcc25bedd9ebbb89d11a2e6d766a34716049005e
twink <twink@DESKTOP-LPCCCN9>
2023-08-24 09:08:11 +0530

    pushed1

---
+++ GitHubPractice/src/test/java/Sample1/DemoTest.java
@@ -0,0 +1,20 @@
+package Sample1;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	 
+		
+		WebDriver driver=null;
+		
+		
+		
+		@Test
+		 public void Test1()
+         {
+           System.out.println("Hey");	
+	     }
+
+}
---
+++ GitHubPractice/src/test/java/Sample1/DemoTest2.java
@@ -0,0 +1,17 @@
+package Sample1;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+
+public class DemoTest2 {
+	
+	WebDriver driver=null;
+	
+	@Test
+	 public void Test2()
+	{
+		System.out.println("Welcome");
+	}
+
+}
